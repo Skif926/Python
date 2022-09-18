4477b4098b762de64d1ffb8802ba283d92ccff1b
Skif926 <skif926@mail.ru>
2022-09-18 22:59:07 +0300

    Initial commit

---
+++ Task2/Task_2.py
@@ -0,0 +1,158 @@
+'''Задание 2
+Руководителю иногда бывает сложно подобрать слова для речи. Помогите ему создав
+генератор случайных фраз из задаваемых слов. Генератор должен работать так: вы
+вводите существительные (по одному на строку), после чего идет “стоп слово” и вы
+пишете глаголы, также до “стоп слова”. После этого количество фраз которые нужно
+сгенерировать. и вывести фразы. с-существительное, г-глагол, типы фраз сгс,гсс,ссг.
+(Фразы могут иметь ошибки в склонениях и падежах)'''
+import math
+import random
+
+
+def startenter(b):
+    str = []
+    while True:
+        print('Введите', b, 'или чтобы закончить введите \'стоп слово\'')
+        a = input()
+        if a == 'стоп слово':
+            return str
+        str.append(a)
+
+
+def outputA(nounl, verbl, *b):
+    if len(b) == 0:
+        str = takestr(nounl) + ' ' + takestr(verbl) + ' ' + takestr(nounl)
+        return str
+    elif len(b) == 1:
+        str = takestr(verbl) + ' ' + takestr(nounl) + ' ' + takestr(nounl)
+        return str
+    elif len(b) == 2:
+        str = takestr(nounl) + ' ' + takestr(nounl) + ' ' + takestr(verbl)
+        return str
+
+
+def outputB(nounl, verbl, *b):
+    strA = []
+    strB = []
+    strA += nounl
+    strB += verbl
+    if len(b) == 0:
+        str = takeuniquestr(strA) + ' ' + takeuniquestr(strB) + ' ' + takeuniquestr(strA)
+        return str
+    elif len(b) == 1:
+        str = takeuniquestr(strB) + ' ' + takeuniquestr(strA) + ' ' + takeuniquestr(strA)
+        return str
+    elif len(b) == 2:
+        str = takeuniquestr(strA) + ' ' + takeuniquestr(strA) + ' ' + takeuniquestr(strB)
+        return str
+
+
+def takestr(str):
+    if len(str) == 1:
+        return str[0]
+    return str[random.randint(0, len(str) - 1)]
+
+
+def takeuniquestr(str):
+    if len(str) == 1:
+        strout = str[0]
+        str.pop(0)
+        return strout
+    c = len(str) - 1
+    a = random.randint(0, c)
+    strout = str[a]
+    str.pop(a)
+    return strout
+
+
+def generation(nounlocal, verblocal, strprintlocal, countlocal, canlocal):
+    brick = 0
+    for i in range(countlocal):
+        while brick < 1000:
+            if brick == 999:
+                print('Pilic')
+            c = random.randint(0, 2)
+            if c == 0:  # сгс
+                if canlocal:
+                    str = outputA(nounlocal, verblocal)
+                else:
+                    str = outputB(nounlocal, verblocal)
+                if str not in strprintlocal:
+                    strprintlocal.append(str)
+                    brick = 0
+                    break
+                else:
+                    brick += 1
+            elif c == 1:  # гсс
+                if canlocal:
+                    str = outputA(nounlocal, verblocal, 1)
+                else:
+                    str = outputB(nounlocal, verblocal, 1)
+                if str not in strprintlocal:
+                    strprintlocal.append(str)
+                    brick = 0
+                    break
+                else:
+                    brick += 1
+            else:  # ссг
+                if canlocal:
+                    str = outputA(nounlocal, verblocal, 1, 2)
+                else:
+                    str = outputB(nounlocal, verblocal, 1, 2)
+                if str not in strprintlocal:
+                    strprintlocal.append(str)
+                    brick = 0
+                    break
+                else:
+                    brick += 1
+
+
+random.seed()
+noun = startenter('существительное')
+verb = startenter('глагол')
+count = 0
+
+random.shuffle(noun)
+random.shuffle(verb)
+countworld = 0
+nounworld = 0
+userinput = input('Могут существительные повторяться в фразе ? Y\\N\n')
+if userinput == 'N' or userinput == 'n' or userinput == '0':
+    can = False
+    for i in range(len(noun)):
+        nounworld += 6 * i
+    countworld = nounworld * len(verb)
+    print('Максимальное количество уникальных фраз без повтора существительных', countworld)
+else:
+    can = True
+    for i in range(len(noun)):
+        nounworld += 6 * i
+        countworld = (nounworld + 3 * len(noun)) * len(verb)
+    print('Максимальное количество неуникальных фраз с повтором существительных', countworld)
+while True:
+    try:
+        count = int(input('Введите количество фраз которые нужно сгенерировать\n'))
+        if count >= 0:
+            break
+    except ValueError:
+        print('Error\n'
+              'Некорректный ввод')
+        continue
+if count > countworld:
+    print('Выход за границы возможностей\n'
+          'Будет составленно:', countworld, 'фраз')
+    count = countworld
+
+strprint = []
+generation(noun, verb, strprint, count, can)
+
+
+
+if count > len(strprint):
+    print('Выход за границы возможностей\n'
+          'Будет составленно:', len(strprint), 'фраз')
+    count = len(strprint)
+
+strprint.sort()
+for i in range(count):
+    print(i + 1, ' - ', strprint[i])
---
+++ pythonProject/Task_1.py
@@ -0,0 +1,142 @@
+'''
+Задание 1
+Для повышения эффективности работы инженера ему нужно быстрее считать. В этом
+ему поможет калькулятор. напишите калькулятор с функциями (+,-,*,/) а так же
+возведение в степень, остаток от деления, извлечение корня. В случае невозможных
+вычислений он должен выдавать “Error”.
+'''
+
+import os
+import time
+from cmath import inf
+
+clear = lambda: os.system('cls')
+addition = lambda x, y: x + y
+subtraction = lambda x, y: x - y
+multiplication = lambda x, y: x * y
+division = lambda x, y: x / y
+exponentiation = lambda x, y: x ** y
+remainderdivision = lambda x, y: x % y
+rootextraction = lambda x: x ** (1 / 2)
+
+
+def userinput(*b):
+    while True:
+        print('Введите число')
+        a = input()
+        if is_digit(a) and len(b) == 0:
+            if float(a) == inf:
+                print('Это число для меня слишком большое, я ведь только начинающий калькулятор')
+                continue
+            return a
+        elif is_digit(a) and len(b) == 1 and float(a) != 0:
+            return a
+        elif a.isdigit() and len(b) == 2:
+            return a
+        elif is_digit(a) and a.isdigit() == False and len(b) == 2:
+            print('Это число отрицательное, в результате будет использовано положительное число')
+            return float(a) * (-1)
+        else:
+            print('Error\n'
+                  'Некорректный ввод')
+
+
+def is_digit(a):
+    try:
+        float(a)
+        return True
+    except ValueError:
+        return False
+
+
+def continuecounting():
+    print('Продолжить вычисления? (1,Y)')
+    a = input()
+    if a == '1' or a == 'y' or a == 'Y':
+        return True
+    else:
+        return False
+
+
+def calcul():
+    print('Привет я калькулятор!\n'
+          'Каждому инженеру нужен калькулятор для повышения эффективности рабоы\n'
+          'Какую операцию выполнить:\n'
+          '1 - Сложение\n'
+          '2 - Вычитание\n'
+          '3 - Умножение\n'
+          '4 - Деление\n'
+          '5 - Возведение в степень\n'
+          '6 - Остаток от деления\n'
+          '7 - Извлечение корня\n'
+          '0 - Выход')
+    strInput = input()
+    if strInput.isdigit() and int(strInput) in range(8):
+        if int(strInput) == 1:
+            clear()
+            print('Операция Cложение A + B ')
+            a = addition(float(userinput()), float(userinput()))
+            if a == inf:
+                print('Error\n'
+                      'Полученное число для меня слишком большое, я ведь только начинающий калькулятор')
+            print('Ответ: ', a)
+            return continuecounting()
+        elif int(strInput) == 2:
+            clear()
+            print('Операция Вычитание A - B ')
+            a = subtraction(float(userinput()), float(userinput()))
+            if a == inf:
+                print('Error\n'
+                      'Полученное число для меня слишком большое, я ведь только начинающий калькулятор')
+            print('Ответ: ', a)
+            return continuecounting()
+        elif int(strInput) == 3:
+            clear()
+            print('Операция Умножение A * B ')
+            a = multiplication(float(userinput()), float(userinput()))
+            if float(a) == inf:
+                print('Error\n'
+                      'Полученное число для меня слишком большое, я ведь только начинающий калькулятор')
+            print('Ответ: ', a)
+            return continuecounting()
+        elif int(strInput) == 4:
+            clear()
+            print('Операция Деление A \\ B ')
+            a = division(float(userinput()), float(userinput(True)))
+            if a == inf:
+                print('Error\n'
+                      'Полученное число для меня слишком большое, я ведь только начинающий калькулятор')
+            print('Ответ: ', a)
+            return continuecounting()
+        elif int(strInput) == 5:
+            clear()
+            print('Операция Возведение в степень A ^ B ')
+            a = exponentiation(float(userinput()), float(userinput()))
+            if a == inf:
+                print('Полученное число для меня слишком большое, я ведь только начинающий калькулятор')
+            print('Ответ: ', a)
+            return continuecounting()
+        elif int(strInput) == 6:
+            clear()
+            print('Операция Остаток от деления A mod B ')
+            print('Ответ: ', remainderdivision(float(userinput(0, 0)), float(userinput(0, 0))))
+            return continuecounting()
+        elif int(strInput) == 7:
+            clear()
+            print('Операция Извлечение корня √A')
+            print('Ответ: ', rootextraction(float(userinput(0, 0))))
+            return continuecounting()
+        elif int(strInput) == 0:
+            return False
+
+    else:
+        print('Error\n'
+              'Некорректный выбор операции')
+
+    return True
+
+
+while calcul():
+    time.sleep(1)
+    clear()
+    continue
